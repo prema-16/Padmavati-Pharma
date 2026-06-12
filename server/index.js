40db82473f3a5d213b8799330afe910a0110b1ca
Premanand Londhe <premanandlondhe16@gmail.com>
2026-06-12 14:34:13 +0530

    fix: add Vercel URL to CORS allowed origins

--- server/index.js
+++ server/index.js
@@ -30,6 +30,8 @@ app.use(cors({
     "http://localhost:5173",
     "http://localhost:5174",
     "http://localhost:5175",
+    "https://padmavati-pharma-e5mzwqqo9-premanand-londhe-s-projects.vercel.app",
+    /\.vercel\.app$/,
     process.env.CLIENT_URL,
   ].filter(Boolean),
   credentials: true,
